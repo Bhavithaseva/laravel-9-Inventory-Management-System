9dd57a395d96e8158cd0aca7d2edfb368eff9c5f
Bhavithaseva <bhavitha.s@trendsofttech.co.in>
2023-01-31 13:08:43 +0530

    32. Admin Profile & Image Update Part 2

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -17,9 +17,9 @@
                             <hr>
                             <h4 class="card-title">User Email : {{ $adminData->email }} </h4>
                             <hr>
-                            <h4 class="card-title">User Name : {{ $adminData->name }} </h4>
+                            <h4 class="card-title">User Name : {{ $adminData->username }} </h4>
                             <hr>
-                            <a href="" class="btn btn-info btn-rounded waves-effect waves-light"> Edit Profile</a>
+                            <a href="{{ route('edit.profile') }}" class="btn btn-info btn-rounded waves-effect waves-light" > Edit Profile</a>
 
                         </div>
                     </div>
